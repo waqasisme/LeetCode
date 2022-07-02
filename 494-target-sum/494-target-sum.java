219594ae1175c30ba668268a0fe5aa2f4ade76fb
Muhammad Waqas Maqsood <waqasisme@gmail.com>
2022-07-02 23:07:57 +0200

    Time: 15 ms (72.38%), Space: 51.6 MB (26.75%) - LeetHub

---
+++ 494-target-sum/494-target-sum.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int findTargetSumWays(int[] nums, int target) {
+        
+        int sum = 0;
+        for(int num: nums) {
+            sum += num; // total max sum
+        }
+        
+        if(sum < target || -sum > target) {
+            return 0; // not possible to reach this sum 
+        }
+        
+        
+        int[] table = new int[2 * sum + 1]; // + 1 to check number of ways to reach sum 0
+        table[sum] = 1; // ways to reach sum or -sum = same sign for all = 1 way
+        
+        for (int i = 0; i < nums.length; i++) {
+            int[] tempTable = new int[2 * sum + 1];
+            for(int j = 0; j < 2 * sum + 1; j++) {
+                if (table[j] != 0) {
+                    tempTable[j + nums[i]] += table[j];
+                    tempTable[j - nums[i]] += table[j];
+                }
+            }
+            table = tempTable;
+        }
+    return table[sum + target];
+    }
+}
