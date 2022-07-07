049d2486f0c602db477cdf3d932a8611445f2457
Muhammad Waqas Maqsood <waqasisme@gmail.com>
2022-07-07 15:20:32 +0200

    Time: 392 ms (5.15%), Space: 84.2 MB (53.69%) - LeetHub

--- 1609-even-odd-tree/1609-even-odd-tree.java
+++ 1609-even-odd-tree/1609-even-odd-tree.java
@@ -21,11 +21,12 @@ class Solution {
     
         // perform level order using q
         LinkedList<TreeNode> currentLevel = new LinkedList();
-        LinkedList<TreeNode> nextLevel = new LinkedList();
         currentLevel.add(root);
         boolean levelEven = true;
         
         while(!currentLevel.isEmpty()) {
+            
+            int size = currentLevel.size();
             
             // check even odd of first item in case of only 1 item in node (and since our loop won't check)
             if ((levelEven && currentLevel.get(0).val % 2 == 0) ||
@@ -33,7 +34,7 @@ class Solution {
                 return false;
             }
                 
-            for(int i = 1; i < currentLevel.size(); i++) {
+            for(int i = 1; i < size; i++) {
                 if (levelEven) { // items must be odd and strictly increasing
                     if(currentLevel.get(i).val % 2 == 0 || currentLevel.get(i).val <= currentLevel.get(i - 1).val) {
                         return false;
@@ -47,16 +48,16 @@ class Solution {
                 }
             }
             
-            nextLevel.clear();
-            while(!currentLevel.isEmpty()) {
+            
+            while(size-- > 0) {
                 TreeNode item = currentLevel.poll();
                 if(item.left != null)
-                    nextLevel.add(item.left);
+                    currentLevel.add(item.left);
                 if(item.right != null)
-                    nextLevel.add(item.right);
+                    currentLevel.add(item.right);
             }
             
-            currentLevel.addAll(nextLevel);
+            // currentLevel.addAll(nextLevel);
             levelEven = !levelEven;
         }
 
