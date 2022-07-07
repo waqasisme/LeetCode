7f6f0b36263bcf9a4d1dbfedc85214904303c8db
Muhammad Waqas Maqsood <waqasisme@gmail.com>
2022-07-07 15:18:15 +0200

    Time: 469 ms (5.15%), Space: 84.3 MB (51.97%) - LeetHub

--- 1609-even-odd-tree/1609-even-odd-tree.java
+++ 1609-even-odd-tree/1609-even-odd-tree.java
@@ -15,55 +15,37 @@
  */
 class Solution {
      
-    public boolean isOdd(int num) {
-        return num %2 != 0;
-    }
-    
-    public boolean isEven(int num) {
-        return num % 2 == 0;
-    }
-    
     public boolean isEvenOddTree(TreeNode root) {
-        if (root == null || isEven(root.val))
+        if (root == null || root.val % 2 == 0)
             return false;
     
         // perform level order using q
         LinkedList<TreeNode> currentLevel = new LinkedList();
         LinkedList<TreeNode> nextLevel = new LinkedList();
         currentLevel.add(root);
-        int level = 0;
-        boolean isEmpty = currentLevel.isEmpty();
-        
+        boolean levelEven = true;
         
         while(!currentLevel.isEmpty()) {
-            for(TreeNode item: currentLevel)
-                System.out.print(item.val + "->");
-            System.out.println("LEVEL " + level + " ITEMS " + currentLevel.size());
             
-            
-            // check even odd of first item in case of only 1 item in node
-            if ((isEven(level) && isEven(currentLevel.get(0).val)) ||
-                ( isOdd(level) &&  isOdd(currentLevel.get(0).val))) {
+            // check even odd of first item in case of only 1 item in node (and since our loop won't check)
+            if ((levelEven && currentLevel.get(0).val % 2 == 0) ||
+                (!levelEven &&  currentLevel.get(0).val % 2 != 0)) {
                 return false;
             }
                 
-            
-            
             for(int i = 1; i < currentLevel.size(); i++) {
-                if (isEven(level)) { // items must be odd and strictly increasing
-                    if(isEven(currentLevel.get(i).val) || currentLevel.get(i).val <= currentLevel.get(i - 1).val) {
+                if (levelEven) { // items must be odd and strictly increasing
+                    if(currentLevel.get(i).val % 2 == 0 || currentLevel.get(i).val <= currentLevel.get(i - 1).val) {
                         return false;
                     }
                         
                 } else { // items must be even and strictly decreasing
-                    if(isOdd(currentLevel.get(i).val) || currentLevel.get(i).val >= currentLevel.get(i - 1).val) {
-                        System.out.println("COMPARING " + currentLevel.get(i).val + " " + currentLevel.get(i - 1).val);
+                    if(currentLevel.get(i).val % 2 != 0 || currentLevel.get(i).val >= currentLevel.get(i - 1).val) {
                         return false;
                     }
                         
                 }
             }
-            
             
             nextLevel.clear();
             while(!currentLevel.isEmpty()) {
@@ -75,7 +57,7 @@ class Solution {
             }
             
             currentLevel.addAll(nextLevel);
-            level++;
+            levelEven = !levelEven;
         }
 
         
